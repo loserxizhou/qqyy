3ea721b7deabcc138ddd6ab22433087113952a56
xiexizhou_glb <xiexizhou_glb@qq.com>
2020-08-03 11:29:29 +0800

    项目的创建

---
+++ src/commons/Tabbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <router-view />
+
+    <van-tabbar route active-color="#eb4d4b">
+      <van-tabbar-item replace to="/home" icon="home-o">
+        首页
+      </van-tabbar-item>
+      <van-tabbar-item replace to="/rank" icon="search">
+        排行
+      </van-tabbar-item>
+      <van-tabbar-item replace to="/mine" icon="search">
+        我的
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="stylus" scoped></style>
---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Tabber from "../commons/Tabbar";
+Vue.use(Router);
+
+export default new Router({
+  mode: "history", // h5模式
+  routes: [
+    {
+      path: "/",
+      component: Tabber,
+      children: [
+        {
+          path: "/home",
+          component: () => import("../pages/home/Home.vue")
+        },
+        {
+          path: "/rank",
+          component: () => import("../pages/rank/Rank.vue")
+        },
+        {
+          path: "/mine",
+          component: () => import("../pages/mine/Mine.vue")
+        }
+      ]
+    }
+  ]
+});
